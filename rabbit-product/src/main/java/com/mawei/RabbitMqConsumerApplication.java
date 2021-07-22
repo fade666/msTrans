69970e23d70e730cd375d48c62e7dabcd45bf67f
MaWei <1136735603@qq.com>
2021-07-22 15:59:24 +0800

    1.完善hystrix熔断代码 2.图形页面监控模块

--- rabbit-product/src/main/java/com/mawei/RabbitMqConsumerApplication.java
+++ rabbit-product/src/main/java/com/mawei/RabbitMqConsumerApplication.java
@@ -3,7 +3,9 @@ package com.mawei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Maweiii
@@ -15,4 +17,5 @@ public class RabbitMqConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(RabbitMqConsumerApplication.class, args);
     }
+
 }
